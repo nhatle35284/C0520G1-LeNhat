e5823483bd8a8a4afa0a7ba96c9031018cdbfef9
nhatle352840 <leenhat11a1nh@gmail.com>
2020-08-26 08:58:29 +0700

    bailam

--- module3/9.Web_over_view/exercise/product_discount_calculator/src/DiscountServlet.java
+++ module3/9.Web_over_view/exercise/product_discount_calculator/src/DiscountServlet.java
@@ -14,13 +14,15 @@ public class DiscountServlet extends HttpServlet {
         String discount = request.getParameter("discount");
 
         float total= (float) (price*percent*0.01);
+        float priceDiscount = price -total;
 
         PrintWriter writer = response.getWriter();
         writer.println("<html>");
-        writer.println("<h1>Mô tả: " + discount+ "</h1>");
-        writer.println("<h1>Giá niêm yết: " + price+ "</h1>");
-        writer.println("<h1>USD: " + percent+ "</h1>");
-        writer.println("<h1>Tổng là: " + total+ "</h1>");
+        writer.println("<h1>Product Description: " + discount+ "</h1>");
+        writer.println("<h1>List Price " + price+ "</h1>");
+        writer.println("<h1>Discount Percent:  " + percent+ "</h1>");
+        writer.println("<h1>Discount Amount: " + total+ "</h1>");
+        writer.println("<h1>Discount Price: " + priceDiscount+ "</h1>");
         writer.println("</html>");
     }
 
